9274dbdf26665e2312a14f39ec219c4bff5537f1
a-nikolay-roonyx <nikolay.anchutin@roonyx.team>
2019-01-16 14:16:52 +0300

    Added changeDetection

--- src/app/shared/components/songs/songs.component.ts
+++ src/app/shared/components/songs/songs.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { ISong } from '@lib/models';
 
 @Component({
   selector: 'app-songs',
   templateUrl: './songs.component.html',
-  styleUrls: ['./songs.component.scss']
+  styleUrls: ['./songs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SongsComponent {
   @Input() songs: ISong[];
